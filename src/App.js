5668e4ac4fb1de55198495affba863878165ed2b
Manoj Mutyala <manojmutyala@outlook.com>
2018-11-23 14:22:09 +0530

    Router and Single Page Implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
+import { Layout, Menu, Row, Col, List, Avatar, Icon } from 'antd';
+import $ from 'jquery';
 import './App.css';
-import MainLayout from './Layout/MainLayout';
+
+const { Header, Content, Footer } = Layout;
+
+const mobile = { xs: 8, sm: 16, md: 24}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowHeight: 0,
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      windowHeight: $(window).height(),
+    });
+  }
+
   render() {
     return (
-        <MainLayout />
-    );
-  }
+      <HashRouter>
+        <Layout className="layout">
+            <Header>
+              <div className="logo">BABAI</div>
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['1']}
+                style={{ lineHeight: '64px' }}
+              >
+                <Menu.Item key="1"><NavLink to="/">Home</NavLink></Menu.Item>
+                <Menu.Item key="2"><NavLink to="/contact">Contact Us</NavLink></Menu.Item>
+              </Menu>
+            </Header>
+            <Content>
+              <div style={{ background: '#fff', padding: 24, minHeight: this.state.windowHeight }}>
+              <div style={{padding: "0 0 20px 0"}}>
+                <Row gutter={mobile}>
+                  <Col xs={24} sm={12} md={6} span={6}><div className="gutter-box">col-6</div></Col>
+                  <Col xs={24} sm={12} md={6} span={6}><div className="gutter-box">col-6</div></Col>
+                  <Col xs={24} sm={12} md={6} span={6}><div className="gutter-box">col-6</div></Col>
+                  <Col xs={24} sm={12} md={6} span={6}><div className="gutter-box">col-6</div></Col>
+                </Row>
+              </div>
+                <Route exact path="/" component={Home}/>
+                <Route path="/contact" component={Contact}/>
+              </div>
+            </Content>
+            <Footer className="footer">
+              babai-ias ©2018 All Rights Received
+            </Footer>
+          </Layout>
+        </HashRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from "react";
+import { Carousel } from "antd";
+
+class Home extends Component {
+    render() {
+      return (
+        <Fragment>
+            <div style={{textAlign: "justify", width: "50%", border: "1px solid red", padding: "10px 20px"}}>
+                <h2>Do not settle for short-term populist measures; focus on long term economic model: Vice President tells States</h2>
+                <p>The Vice President of India, Shri M. Venkaiah Naidu has said that states must focus on long term economic model for the betterment of the people and must not settle for short-term populist measures.</p>
+                
+                <p>Addressing the gathering after presenting India Today State of State Awards 2018, here today, he said that the relationship between the states and the centre must be built on trust, transparency and mutual respect.</p>
+                
+                <p>Shri Naidu said both parties should practice openness and should not be limited by narrow political divisiveness. He further said that there is an agenda greater than politics before all of us, the agenda of taking development to the last man, the goal of ensuring Antyodaya. Federals should not end at door steps of State capitals, they must reach to villages, he added.</p>
+                
+                <p>The Vice President said that political differences must end with the elections and there after the ruling party must not be bogged down by dilatory tactics and the opposition parties must be given the space to offer constructive criticism in the legislature. He further said that a measure of accommodative openness must be practiced by both the centre and the states if we were to build a strong team India where every voice has its own space.</p>
+    
+            </div>
+        </Fragment>
+      );
+    }
+  }
+   
+export default Home;
